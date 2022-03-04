89e3e85463305848b81cf2f83102bd4ef2147892
Hlavicka <Hlavicka@DESKTOP-EIH729J>
2022-03-04 17:12:11 +0100

    Commit 20220304

---
+++ PubSysLayout/Server/Controllers/ModuleSettingsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PubSysLayout.Shared.Model;
+
+namespace PubSysLayout.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModuleSettingsController : ControllerBase
+    {
+        private readonly LayoutDBContext _context;
+
+        public ModuleSettingsController(LayoutDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ModuleSettings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ModuleSetting>>> GetModuleSettings(int? id_module, int? id_moduleusage)
+        {
+            var res = _context.ModuleSettings.AsQueryable();
+
+            if (id_module != null && id_module != 0)
+            {
+                res = res.Where(ms => ms.IdModule == id_module.Value);
+            }
+
+            if (id_moduleusage != null && id_moduleusage != 0)
+            {
+                res = res.Where(ms => ms.IdModuleusage == id_moduleusage.Value);
+            }
+
+            return await res.ToListAsync();
+        }
+
+        // GET: api/ModuleSettings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ModuleSetting>> GetModuleSetting(int id)
+        {
+            var moduleSetting = await _context.ModuleSettings.FindAsync(id);
+
+            if (moduleSetting == null)
+            {
+                return NotFound();
+            }
+
+            return moduleSetting;
+        }
+
+        // PUT: api/ModuleSettings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModuleSetting(int id, ModuleSetting moduleSetting)
+        {
+            if (id != moduleSetting.IdSetting)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(moduleSetting).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModuleSettingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ModuleSettings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ModuleSetting>> PostModuleSetting(ModuleSetting moduleSetting)
+        {
+            _context.ModuleSettings.Add(moduleSetting);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetModuleSetting", new { id = moduleSetting.IdSetting }, moduleSetting);
+        }
+
+        // DELETE: api/ModuleSettings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModuleSetting(int id)
+        {
+            var moduleSetting = await _context.ModuleSettings.FindAsync(id);
+            if (moduleSetting == null)
+            {
+                return NotFound();
+            }
+
+            _context.ModuleSettings.Remove(moduleSetting);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ModuleSettingExists(int id)
+        {
+            return _context.ModuleSettings.Any(e => e.IdSetting == id);
+        }
+    }
+}
