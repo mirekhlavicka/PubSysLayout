d126696ada09911497eb02ca859ba9ef1df42a70
Hlavicka <Hlavicka@DESKTOP-EIH729J>
2022-03-09 16:56:38 +0100

    Commit20220309

--- PubSysLayout/Client/Program.cs
+++ PubSysLayout/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 using PubSysLayout.Client;
 
@@ -23,6 +24,17 @@ builder.Services.AddTransient/*Scoped*/(sp =>
     return http;
 });
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+    //config.SnackbarConfiguration.PreventDuplicates = false;
+    //config.SnackbarConfiguration.NewestOnTop = false;
+    //config.SnackbarConfiguration.ShowCloseIcon = true;
+    //config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    //config.SnackbarConfiguration.HideTransitionDuration = 500;
+    //config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    //config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 await builder.Build().RunAsync();
