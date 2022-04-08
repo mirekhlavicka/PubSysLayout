edd19a9a6a87976782be27021dea292bfab1b0a8
Mirek <mirekhlavicka@gmail.com>
2022-04-09 00:10:56 +0200

    Commit20220408

--- PubSysLayout/Server/Controllers/ModuleUsagesController.cs
+++ PubSysLayout/Server/Controllers/ModuleUsagesController.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PubSysLayout.Shared.Model;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PubSysLayout.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ModuleUsagesController : ControllerBase
     {
         private readonly LayoutDBContext _context;
