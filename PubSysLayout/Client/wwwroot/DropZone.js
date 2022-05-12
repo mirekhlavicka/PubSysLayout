9adddac78e7dc6705073436e4a246b3e32fb1e4c
Hlavicka <Hlavicka@DESKTOP-EIH729J>
2022-05-12 17:24:49 +0200

    Commit20220512

---
+++ PubSysLayout/Client/wwwroot/DropZone.js
@@ -0,0 +1,48 @@
+﻿export function initializeFileDropZone(element, inputFile) {
+    function pasteHandler(e) {
+        inputFile.files = e.clipboardData.files;
+        const event = new Event('change', { bubbles: true });
+        inputFile.dispatchEvent(event);
+    }
+
+    function onDrop(e) {
+        e.preventDefault();
+        element.classList.remove("hover");
+
+        inputFile.files = e.dataTransfer.files;
+        const event = new Event('change', { bubbles: true });
+        inputFile.dispatchEvent(event);
+    }
+
+    function addDragClass (e) {
+        e.preventDefault();
+        element.classList.add("hover");
+    }
+
+    function removeDragClass (e) {
+        e.preventDefault();
+        element.classList.remove("hover");
+    }
+
+    if (element == null) {
+        return {
+            dispose: () => { }
+        };
+    }
+
+    element.addEventListener('paste', pasteHandler);
+    element.addEventListener("dragenter", addDragClass);
+    element.addEventListener("dragover", addDragClass);
+    element.addEventListener("dragleave", removeDragClass);
+    element.addEventListener("drop", onDrop);
+
+    return {
+        dispose: () => {
+            element.removeEventListener('paste', pasteHandler);
+            element.removeEventListener('dragenter', addDragClass);
+            element.removeEventListener('dragover', addDragClass);
+            element.removeEventListener('dragleave', removeDragClass);
+            element.removeEventListener("drop", onDrop);
+            }
+    }
+}
