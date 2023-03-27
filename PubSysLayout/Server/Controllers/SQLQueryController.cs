06e2be3f7bf4ce11f39023de89528844b80991b0
Hlavicka <Hlavicka@DESKTOP-EIH729J>
2023-03-27 17:21:36 +0200

    Save SQL query

--- PubSysLayout/Server/Controllers/SQLQueryController.cs
+++ PubSysLayout/Server/Controllers/SQLQueryController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using System.Data;
 using PubSysLayout.Shared.SQLQuery;
+using Query = PubSysLayout.Shared.SQLQuery.Query;
 
 namespace PubSysLayout.Server.Controllers
 {
@@ -12,15 +13,17 @@ namespace PubSysLayout.Server.Controllers
     public class SQLQueryController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _environment;
         private int maxRowCount = 1000;
-        public SQLQueryController(IConfiguration configuration)
+        public SQLQueryController(IConfiguration configuration, IWebHostEnvironment environment)
         {
             _configuration = configuration;
+            _environment = environment;
             maxRowCount = _configuration.GetValue<int>("SQLQuery:maxRowCount");
         }
 
         [HttpPost]
-        public IActionResult Run(PubSysLayout.Shared.SQLQuery.Query query)
+        public IActionResult Run(Query query)
         {
             using (var conn = new SqlConnection(String.Format(_configuration.GetConnectionString("PubSysDefault"), query.Database)))
             {
@@ -49,6 +52,67 @@ namespace PubSysLayout.Server.Controllers
                     return BadRequest(ex.Message);
                 }
             }
+        }
+
+        [HttpPost("save")]
+        public IActionResult Save(Query query, string name)
+        {
+            var directoryName = _configuration.GetValue<string>("SQLQuery:savedSQLDirectoryName");
+            var directoryPath = Path.Combine(_environment.ContentRootPath, directoryName);
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            if (!String.IsNullOrEmpty(name))
+            {
+                try
+                {
+                    string SQL = System.IO.File.ReadAllText(Path.Combine(directoryPath, $"{name}.sql"));
+
+                    if (SQL == query.SQL)
+                    {
+                        return Ok(name);
+                    }
+                    else
+                    {
+                        name = string.Empty;
+                    }
+                }
+                catch
+                { 
+                    name = string.Empty;
+                }
+            }
+
+            if (String.IsNullOrEmpty(name))
+            {
+                name = Guid.NewGuid().ToString("n").Substring(0, 12);
+            }
+
+            System.IO.File.WriteAllText(Path.Combine(directoryPath, $"{name}.sql"), query.SQL);
+
+            return Ok(name);
+        }
+
+        [HttpGet("savedsql")]
+        public string GetSavedSQL(string name)
+        {
+            var directoryName = _configuration.GetValue<string>("SQLQuery:savedSQLDirectoryName");
+            var directoryPath = Path.Combine(_environment.ContentRootPath, directoryName);
+            if (!Directory.Exists(directoryPath))
+            {
+                return "";
+            }
+
+            string SQL = "";
+            try
+            {
+                SQL = System.IO.File.ReadAllText(Path.Combine(directoryPath, $"{name}.sql"));
+            }
+            catch
+            { }
+            return SQL;
         }
 
         [HttpGet("dblist")]
