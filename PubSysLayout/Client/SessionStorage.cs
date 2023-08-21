d58a147a2f5f30af4b3e41ca0b0306707e7574a0
Hlavicka <Hlavicka@DESKTOP-EIH729J>
2023-08-21 15:33:07 +0200

    SQL query builder: save Session to Local storage

--- PubSysLayout/Client/SessionStorage.cs
+++ PubSysLayout/Client/SessionStorage.cs
@@ -1,8 +1,16 @@
-﻿namespace PubSysLayout.Client
+﻿using System.Text.Json;
+
+namespace PubSysLayout.Client
 {
     public class SessionStorage
     {
-        private Dictionary<string, object> items = new Dictionary<string, object>();
+        private Dictionary<string, object> items = null;
+        private readonly LocalStorage localStorage;
+
+        public SessionStorage(LocalStorage localStorage)
+        {
+            this.localStorage = localStorage;
+        }
 
         public Dictionary<string, object> Items
         {
@@ -16,11 +24,39 @@
         {
             get 
             { 
-                return items.GetValueOrDefault(key); 
+                return items?.GetValueOrDefault(key); 
             }
             set 
-            { 
-                items[key] = value; 
+            {
+                if (items != null)
+                {
+                    items[key] = value;
+                }
+            }
+        }
+
+        public async Task Save()
+        {
+            await localStorage.SetAsync("Session", items);
+            await localStorage.SetAsync("Session_Types", items.ToDictionary(i => i.Key, i => i.Value != null ? i.Value.GetType().FullName : "null"));
+        }
+
+        public async Task Load()
+        {
+            if (items == null)
+            {
+                items = await localStorage.GetAsync<Dictionary<string, object>>("Session");
+                Dictionary<string, string> types = await localStorage.GetAsync<Dictionary<string, string>>("Session_Types");
+
+                if(items != null  && types != null && items.Count == types.Count) 
+                {
+                    foreach (var kv in items.ToArray()) 
+                    {
+                        items[kv.Key] = types[kv.Key] == "null" ? null : ((JsonElement)(kv.Value)).Deserialize(Type.GetType(types[kv.Key]));
+                    }
+                }
+
+                items ??= new Dictionary<string, object>();
             }
         }
     }
